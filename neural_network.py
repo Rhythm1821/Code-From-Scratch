5c46efbe48d25f4355a3eef3db8472a6a8bab1df
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-22 12:15:04 +0530

    Backward and train function

--- neural_network.py
+++ neural_network.py
@@ -16,4 +16,30 @@ class NeuralNetwork:
         loss = -(self.y*np.log(y_pred) + (1 - self.y) * np.log(1 - y_pred))
         return loss.mean()
     def backward(self):
-        pass
+        y_pred = self.forward()
+        m = self.X.shape[0]
+
+        # Compute gradient using the chain rule
+        dL_dy_pred = -(self.y / y_pred) + ((1 - self.y) / (1 - y_pred))
+        dy_pred_dz = y_pred * (1 - y_pred)
+        dL_dz = dL_dy_pred * dy_pred_dz
+
+        # Compute gradients with respect to weights and biases
+        dL_dw = (1 / m) * np.dot(self.X.T, dL_dz)
+        dL_db = (1 / m) * np.sum(dL_dz, axis=1, keepdims=True)
+        
+        return dL_dw, dL_db
+    
+    def train(self,epochs,lr):
+        for epoch in range(epochs):
+            y_pred = self.forward()
+            loss = self.loss()
+            dL_dw, dL_db = self.backward()
+            print(self.weights)
+            print(dL_dw)
+            # print()
+            self.weights -= lr * dL_dw
+            self.bias -= lr * dL_db
+
+            #  print the loss for monitoring training progress
+            print(f"Epoch {epoch+1}/{epochs}, Loss: {loss:.4f}")        
