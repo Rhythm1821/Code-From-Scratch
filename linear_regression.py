0b0598b7615e237143ff958566148dcbc43e8976
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-24 10:58:28 +0530

    Defining arguments

--- linear_regression.py
+++ linear_regression.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self):
-        pass
+    def __init__(self,lr=0.01,iterations=1000):
+        self.lr = lr
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
     def fit(self):
         pass
     def predict(self):
