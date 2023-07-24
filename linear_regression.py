2cf214cc7aca863ade6199aa24708ea1c256a4bf
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-24 11:38:20 +0530

    linear_regression final prediction and evaluating the loss

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+from sklearn.datasets import make_regression
 
 class LinearRegression:
     def __init__(self,lr=0.01,iterations=1000):
@@ -28,3 +31,16 @@ class LinearRegression:
             
     def predict(self,X):
         return np.dot(X,self.weights) + self.bias
+
+X,y = make_regression(n_samples=100,
+                        n_features=2,
+                        noise=1,random_state=42)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+y_pred = lr.predict(X_test)
+
+mse = mean_squared_error(y_pred=y_pred,y_true=y_test)
+mae = mean_absolute_error(y_pred=y_pred,y_true=y_test)
+print(mse,mae)
