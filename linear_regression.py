1f77e664caee734d027c5910778f652fedda67a8
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-24 11:05:52 +0530

    Adding the fit method

--- linear_regression.py
+++ linear_regression.py
@@ -6,7 +6,25 @@ class LinearRegression:
         self.iterations = iterations
         self.weights = None
         self.bias = None
-    def fit(self):
-        pass
+    def fit(self,X,y):
+        num_samples, num_features = X.shape
+
+        # Initialize the weights and bias to zeros
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        # Gradient descent
+        for _ in range(self.iterations):
+            # Predict the output using the weights and bias 
+            y_pred = self.predict(X)
+
+            # Calculate the gradients
+            dw = (1 / num_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / num_samples) * np.sum(y_pred - y)
+
+            # Update the weights and biases
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+            
     def predict(self):
         pass
