610e9f5389189058ea5219b2ab5ab4cf36322561
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-25 12:30:54 +0530

    logistic_regression basic structure

---
+++ logistic_regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self):
+        pass
+    def fit(self,X,y):
+        pass
+    def predict(self,x):
+        pass
+    def sigmoid(self,z):
+        return 1 / 1 - np.exp(-z)
