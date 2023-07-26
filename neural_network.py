946b871f402645244c9b96418c92d6dd409be9bc
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-26 13:21:21 +0530

    neural network

--- neural_network.py
+++ neural_network.py
@@ -7,7 +7,7 @@ class NeuralNetwork:
         self.output_size= output_size
 
         # Initialize weights and biases for hidden layer
-        self.W1 = np.random.randn(self.input_size,self.output_size)
+        self.W1 = np.random.randn(self.input_size,self.hidden_size)
         self.b1 = np.zeros((1,self.hidden_size))
 
         # Initilize weights and biases for the output layer
@@ -16,45 +16,69 @@ class NeuralNetwork:
 
     def forward(self,X):
         # forward propagation
-        self.z1 = np.dot(X,self.W1) + self.b1
-        self.a1 = self.sigmoid(self.z1)
-        self.z2 = np.dot(self.a1,self.W2) + self.b2
-        self.a2 = self.softmax(self.z2)
-        return self.a2
+        self.hidden_output = self.sigmoid(np.dot(X,self.W1) + self.b1)
+        self.output = self.sigmoid(np.dot(self.hidden_output,self.W2) + self.b2)
+
+        return  self.output
     
-    def backward(self,X,y,learning_rate):
-        m = X.shape
-
-        # Output layer gradients
-        dZ2 = self.a2 - y
-        dW2 = np.dot(self.a1.T,dZ2) / m
-        db2 = np.sum(dZ2,axis=0,keepdims=True) / m
-
-        # Hidden layer gradients
-        dZ1 = np.dot(dZ2,self.W2.T) * self.sigmoid_derivative(self.z1)
-        dW1 = np.dot(X.T,dZ1) / m
-        db1 = np.sum(dZ1,axis=0,keepdims=True) / m
+    def backward(self,X,y,lr):
+        d_output = (y - self.output) * self.sigmoid_derivative(self.hidden_output)
+        d_W2 = np.dot(self.hidden_output.T,d_output)
+        d_b2 = np.sum(d_output,axis=1,keepdims=True)
+        
+        d_hidden = np.dot(d_output,self.W2.T) * self.sigmoid_derivative(self.hidden_output)
+        d_W1 = np.dot(X.T,d_hidden)
+        d_b1 = np.sum(d_hidden,axis=1,keepdims=True)
 
         # Update weights and biases
-        self.W2 -= learning_rate * dW2
-        self.b2 -= learning_rate * db2
-        self.W1 -= learning_rate * dW1
-        self.b1 -= learning_rate * db1
+        self.W2 += lr * d_W2
+        self.b2 += lr * d_b2
+        self.W1 += lr * d_W1
+        self.b1 += lr * d_b1
 
     def sigmoid(self,z):
         return 1 / (1 + np.exp(-z))
     
     def sigmoid_derivative(self,z):
-        return self.sigmoid(z) * (1 - self.sigmoid(z))
+        return z * (1 - z)
     
-    def softmax(self,z):
-        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
-        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
-    
-    def train(self,X,y,epochs,learning_rate):
-        for epoch in range(epochs):
+    def train(self,X,y,epochs,lr):
+        for _ in range(epochs):
             output = self.forward(X)
-            self.backward(X, y, learning_rate)
+            self.backward(X, y, lr)
+            loss = np.mean((y - output)**2)
     
     def predict(self,X):
-        return np.argmax(self.forward(self.forward(X), axis=1))
+        return self.forward(X)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
