3beeb471eb3f31adb393acf66509995cc7433805
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-30 14:37:46 +0530

    Final touch

--- naive_bayes.py
+++ naive_bayes.py
@@ -19,4 +19,22 @@ class NaiveBayes:
             self._var[c,:] = X_c.var(axis=0)
             self._priors[c,:] = X_c.shape[0] / float(num_samples)
     def predict(self,X):
-        pass
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
+    def _predict(self,x):
+        posteriors = []
+
+        for idx,c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            class_conditional = np.sum(np.log(self._pdf(idx,x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
+
+            return self._classes[np.argmax(posteriors)]
+    def _pdf(self,class_idx,x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(-(x - mean)**2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+
+        return numerator / denominator
